577b7c91cea078fa522056c37f73572db2c13509
chandrika d j <chandrika d j@DESKTOP-Q6S0088>
2022-10-17 18:10:32 +0530

    commit

---
+++ SingleIngeritance/src/com/cg/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,34 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();//EMPLOYEE CLASS OBJECT
+		employee.setName("Anitha");
+		employee.setSalary(15000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();//MANAGER CLASS OBJECT
+		manager.setName("Anish");
+		manager.setSalary(20000);
+		manager.setDepartmentName("HR");
+		em.persist(manager);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
